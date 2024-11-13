a9874e862dd51a8e7e881928188d42c5518e65f3
Amogh Munikote <amogh.munikote@gmail.com>
2024-11-12 22:08:57 -0800

    Replaced comma connect links with Springer Portal

--- selfdrive/ui/qt/widgets/prime.cc
+++ selfdrive/ui/qt/widgets/prime.cc
@@ -100,9 +100,9 @@ PairingPopup::PairingPopup(QWidget *parent) : DialogBase(parent) {
         <li style='margin-bottom: 50px;'>%2</li>
         <li style='margin-bottom: 50px;'>%3</li>
       </ol>
-    )").arg(tr("Go to https://connect.comma.ai on your phone"))
+    )").arg(tr("Go to https://portal.springerelectronics.com on your phone"))
     .arg(tr("Click \"add new device\" and scan the QR code on the right"))
-    .arg(tr("Bookmark connect.comma.ai to your home screen to use it like an app")), this);
+    .arg(tr("Bookmark portal.springerelectronics.com to your home screen to use it like an app")), this);
 
     instructions->setStyleSheet("font-size: 47px; font-weight: bold; color: black;");
     instructions->setWordWrap(true);
